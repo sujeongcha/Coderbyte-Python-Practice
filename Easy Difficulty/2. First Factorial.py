f4c259cd02ef1c2a776a4b093259ce88da8bc5e5
Sue Sujeong Cha <39662008+sujeongcha@users.noreply.github.com>
2019-10-04 20:39:31 -0400

    First Factorial.py

--- Easy Difficulty/2. First Factorial.py
+++ Easy Difficulty/2. First Factorial.py
@@ -2,12 +2,14 @@
 #For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. 
 #For the test cases, the range will be between 1 and 18 and the input will always be an integer.
 
+#My Solution
 def FirstFactorial(num): 
   factor = 1
   for i in range(num):
      factor *= num-i 
   return factor
   
+#Best Solution - using a function inside a function! clever!
 def FirstFactorial(num): 
     if num == 1:
       return 1
