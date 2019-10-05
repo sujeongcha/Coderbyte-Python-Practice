aab130599e9af13f4c36649026a8b3ee9cd70fd9
Sue Sujeong Cha <39662008+sujeongcha@users.noreply.github.com>
2019-10-04 21:20:42 -0400

    Longest Word.py
    str.isalpha() returns “True” if all characters in the string are alphabets

---
+++ Easy Difficulty/3. Longest Word.py
@@ -0,0 +1,25 @@
+#Have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string. 
+#If there are two or more words that are the same length, return the first word from the string with that length. 
+#Ignore punctuation and assume sen will not be empty.
+
+#My Solution
+def LongestWord(sen): 
+  new = ""
+  for letter in sen:
+    if letter.isalpha():
+      new += letter
+    else:
+      new += " "
+  arr = new.split()
+  max_word = max(arr, key=len)
+  return max_word
+  
+#Best Solution    ** str.isalpha() returns “True” if all characters in the string are alphabets
+def LongestWord(sen):
+    nw = ""
+    for letter in sen:
+      if letter.isalpha() or letter.isnumeric():
+        nw += letter
+      else :
+        nw += " "
+    return max(nw.split(),key=len)
